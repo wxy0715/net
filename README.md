accb44cabe57c524f8d2ff79c0f0cfefe7b61671
wxy <1743657883@qq.com>
2024-04-01 13:04:19 +0800

    1

--- README.md
+++ README.md
@@ -4,11 +4,17 @@
 
 需要有一台公网服务器
 
+配置文件提供了默认使用的端口,如果存在端口占用需要自行修改源码重新打包
+
 ## 基于socket版本的http内网穿透
 
 ### 使用的模块
 
 ![image-20240401111552429](https://wxy-md.oss-cn-shanghai.aliyuncs.com/image-20240401111552429.png)
+
+### jar包
+
+![image-20240401130210823](https://wxy-md.oss-cn-shanghai.aliyuncs.com/image-20240401130210823.png)
 
 ### 服务端
 
@@ -22,31 +28,34 @@ http:
   timeout: 1800000
 ```
 
-**启动方式:** 下载源码 进行mvn clean instal 编译后文件http-socket-server.jar上传到linux服务器(也可以本地启动测试)
+**启动方式:** 下载http-socket-server.jar上传到linux服务器(也可以本地启动测试)
 
 **服务器启动命令:** java -jar http-socket-server.jar
 
-![image-20240401112530552](https://wxy-md.oss-cn-shanghai.aliyuncs.com/image-20240401112530552.png)
+![image-20240401124439185](https://wxy-md.oss-cn-shanghai.aliyuncs.com/image-20240401124439185.png)
 
 ### 客户端
 
-```properties
-# 服务端ip
-serverIp=127.0.0.1
-# 服务端端口
-serverPort=19999
-# 内网程序ip
-localIp=127.0.0.1
-# 内网程序端口
-localPort=8000
-```
-
 启动客户端
 
-![image-20240401112726615](https://wxy-md.oss-cn-shanghai.aliyuncs.com/image-20240401112726615.png)
+```properties
+命令: java -jar http-socket-client.jar  服务端ip 服务端端口 内网程序ip 内网程序端口
+示例:java -jar http-socket-client.jar 127.0.0.1 19999 127.0.0.1 8000
+```
 
-访问代理端口
+![image-20240401125915392](https://wxy-md.oss-cn-shanghai.aliyuncs.com/image-20240401125915392.png)
 
-![image-20240401123548421](https://wxy-md.oss-cn-shanghai.aliyuncs.com/image-20240401123548421.png)
+访问客户端程序返回的代理地址
+
+![image-20240401125843909](https://wxy-md.oss-cn-shanghai.aliyuncs.com/image-20240401125843909.png)
 
 ## 基于netty版本http内网穿透-todo
+
+
+
+## 基于netty版本的tcp内网穿透-todo
+
+
+
+## 基于netty版本的udp内网穿透-todo
+
--- http-socket-client/src/main/java/com/net/http/client/Client.java
+++ http-socket-client/src/main/java/com/net/http/client/Client.java
@@ -9,7 +9,6 @@ import com.net.common.util.CloseableUtils;
 import com.net.common.util.HttpMessageUtil;
 import com.net.common.util.HttpUtils;
 import com.net.common.util.ThreadPoolFactory;
-import com.net.http.client.config.ServerConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,10 +25,21 @@ import java.util.concurrent.TimeUnit;
  */
 public class Client extends Socket{
     private static final Logger log = LoggerFactory.getLogger(Client.class);
-
-    private static final ServerConfig serverConfig = ServerConfig.getInstance();
+    // 服务端ip
+    private static String serverIp;
+    // 服务端端口
+    private static int serverPort;
+    // 内网程序ip
+    private static String localIp;
+    // 内网程序端口
+    private static int localPort;
     public static void main(String[] args) {
-        try (Socket clientSocket = new Socket(serverConfig.getServerIp(), serverConfig.getServerPort())){
+        // 读取用户输入的地址
+        serverIp = Optional.of(args).filter(a -> a.length > 0).map(a -> a[0]).orElseThrow(()->new NetRuntimeException("请输入正确的服务端ip"));
+        serverPort = Integer.parseInt(Optional.of(args).filter(a -> a.length > 1).map(a -> a[1]).orElseThrow(() -> new NetRuntimeException("请输入正确的服务端端口")));
+        localIp = Optional.of(args).filter(a -> a.length > 2).map(a -> a[2]).orElseThrow(()->new NetRuntimeException("请输入正确的内网服务ip"));
+        localPort = Integer.parseInt(Optional.of(args).filter(a -> a.length > 3).map(a -> a[3]).orElseThrow(()->new NetRuntimeException("请输入正确的内网服务端口")));
+        try (Socket clientSocket = new Socket(serverIp, serverPort)){
             // 与服务的进行握手
             HttpMessageUtil.sendMessage(clientSocket.getOutputStream(), () -> new Message(MessageType.NEW_SOCKET, Constant.CONNECT.getBytes(StandardCharsets.UTF_8), new HashMap<>()));
             // 读取服务端返回消息
@@ -40,7 +50,7 @@ public class Client extends Socket{
             }
             // 超时时间(如果服务器与客户端长时间未通信这客户端自动关闭连接)
             clientSocket.setSoTimeout(Integer.parseInt(message.getHeaderMap().get(Constant.TIME_OUT)));
-            log.info("代理地址为-> http://{}:{}:超时时间为-> {}毫秒",serverConfig.getServerIp(),message.getHeaderMap().get(Constant.PORT),Integer.parseInt(message.getHeaderMap().get(Constant.TIME_OUT)));
+            log.info("代理地址为-> http://{}:{}:超时时间为-> {}毫秒",serverIp,message.getHeaderMap().get(Constant.PORT),Integer.parseInt(message.getHeaderMap().get(Constant.TIME_OUT)));
             // 定时检测内网服务是否可用,主要防止客户端一直占用服务器资源
             scheduleConnectionListener(clientSocket);
             // 监听服务端的消息
@@ -62,12 +72,12 @@ public class Client extends Socket{
                 // 读取服务端消息
                 Message message = HttpMessageUtil.readMessage(clientSocket.getInputStream());
                 // 监听到消息就进行转发
-                ThreadPoolFactory.execute(new ForwardMessageRunnable(serverConfig.getLocalIp(), serverConfig.getLocalPort(), message, clientSocket));
+                ThreadPoolFactory.execute(new ForwardMessageRunnable(localIp, localPort, message, clientSocket));
             } catch (SocketTimeoutException timeoutException) {
-                log.error("与服务端[{}]长时间未通信连接自动断开", serverConfig.getServerIp() + ":" + serverConfig.getServerPort());
+                log.error("与服务端[{}]长时间未通信连接自动断开", serverIp + ":" + serverIp);
                 break;
             } catch (Exception e) {
-                log.error("与服务端[{}:{}]断开连接,原因:{}", serverConfig.getServerIp(), serverConfig.getServerPort(), e.getMessage());
+                log.error("与服务端[{}:{}]断开连接,原因:{}", serverIp, serverPort, e.getMessage());
                 break;
             }
         }
@@ -80,11 +90,11 @@ public class Client extends Socket{
      */
     public static void scheduleConnectionListener(Socket clientSocket) {
         ThreadPoolFactory.scheduleAtFixedRate(() -> {
-            boolean validate = HttpUtils.validate(serverConfig.getLocalIp(), serverConfig.getLocalPort());
+            boolean validate = HttpUtils.validate(localIp, localPort);
             if (validate) {
                 return;
             }
-            log.info("内网服务{}:{}不可用断开连接！", serverConfig.getLocalIp(), serverConfig.getLocalPort());
+            log.info("内网服务{}:{}不可用断开连接！", localIp, localPort);
             CloseableUtils.close(clientSocket);
         }, 3, 60, TimeUnit.SECONDS);
     }
